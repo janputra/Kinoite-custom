0c28d3de9c833d547cd8bbb0af80acff94cb15ff
Jan Sembiring <jan.s@tetradyne.co.kr>
2024-05-25 00:32:49 +0900

    add cosign.pub & edit build.sh

--- build.sh
+++ build.sh
@@ -11,7 +11,6 @@ RELEASE="$(rpm -E %fedora)"
 # RPMfusion repos are available by default in ublue main images
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
-
 # this installs a package from fedora repos
 rpm-ostree install screen virt-manager samba fcitx5 fcitx5-hangul
 
@@ -21,6 +20,17 @@ rpm-ostree install screen virt-manager samba fcitx5 fcitx5-hangul
 #### Example for enabling a System Unit File
 
 systemctl enable podman.socket
-
-
+systemctl disable waydroid-workaround.service 
+systemctl disable tailscaled.service
+systemctl disable brew-dir-fix.service 
+systemctl disable brew-setup.service 
+systemctl disable brew-upgrade.timer
+systemctl disable brew-update.timer 
 ## remove packages
+rpm-ostree override remove cockpit-networkmanager cockpit-podman cockpit-selinux cockpit-system cockpit-navigator cockpit-storaged
+rpm-ostree override remove lutris waydroid obs-vkcapture.x86_64 obs-vkcapture.i686 cage wlr-randr
+rpm-ostree override remove fluidsynth fluid-soundfont-gm qsynth wxGTK libFAudio wine-core.x86_64 wine-core.i686 wine-pulseaudio.x86_64 wine-pulseaudio.i686 winetricks protontricks
+rpm-ostree override remove tailscale fish xdotool webapp-manager wmctrl steamdeck-kde-presets-desktop   
+rm /usr/bin/waydroid-choose-gpu
+rm -R /home/linuxbrew/.linuxbrew
+rm /usr/share/applications/winetricks.desktop 
