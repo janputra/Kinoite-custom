a34fd6b977f1d7f7adcae682203fe4a49bbe5454
Jan Sembiring <jan.s@tetradyne.co.kr>
2024-05-26 19:27:30 +0900

    remove copr repos

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ RELEASE="$(rpm -E %fedora)"
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 # this installs a package from fedora repos
 rpm-ostree install screen virt-manager samba fcitx5 fcitx5-hangul
-rpm-ostree install steam mangohud gamemode
+rpm-ostree install steam mangohud gamemode 
 
 
 # this would install a package from rpmfusion
