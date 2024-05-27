569f55f5b2fa877c841dc9b57fc3237b3e6a0ac7
Jan Sembiring <janpoetra22@gmail.com>
2024-05-27 14:17:33 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -14,7 +14,7 @@ RELEASE="$(rpm -E %fedora)"
 # this installs a package from fedora repos
 rpm-ostree install screen virt-manager samba fcitx5 fcitx5-hangul
 rpm-ostree install steam mangohud gamemode 
-
+rpm-ostree install plasma-workspace-x11
 
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
