15a9dc53df06e9ea32f8ef1f312f3cf6e3a1cd6b
Jan Sembiring <jan.s@tetradyne.co.kr>
2024-07-07 02:14:03 +0900

    fix KERNEL_SUFFIX

--- build.sh
+++ build.sh
@@ -11,6 +11,13 @@ if [ ! -f /usr/libexec/rpm-ostree/wrapped/dracut ]; then
     rpm-ostree cliwrap install-to-root /
 fi
 
+if [[ "${KERNEL_FLAVOR}" == "surface" ]]; then
+    KERNEL_SUFFIX="surface"
+else
+    KERNEL_SUFFIX=""
+fi
+
+
 QUALIFIED_KERNEL="$(rpm -qa | grep -P 'kernel-(|'"$KERNEL_SUFFIX"'-)(\d+\.\d+\.\d+)' | sed -E 's/kernel-(|'"$KERNEL_SUFFIX"'-)//')"
 
 /usr/libexec/rpm-ostree/wrapped/dracut --no-hostonly --kver "$QUALIFIED_KERNEL" --reproducible -v --add ostree -f "/lib/modules/$QUALIFIED_KERNEL/initramfs.img"
