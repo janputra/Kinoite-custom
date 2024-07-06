3847e436d947dd425b52e3f8a7a59ae9ad9af795
Jan Sembiring <jan.s@tetradyne.co.kr>
2024-07-07 02:00:27 +0900

    add NVreg_EnableGpuFirmware=0

--- build.sh
+++ build.sh
@@ -3,8 +3,19 @@
 set -ouex pipefail
 
 RELEASE="$(rpm -E %fedora)"
+# Universal Blue specific Initramfs fixes
+echo "options nvidia NVreg_EnableGpuFirmware=0" >> /usr/lib/modprobe.d/nvidia-atomic.conf
+sed -i 's@omit_drivers@force_drivers@g' /usr/lib/dracut/dracut.conf.d/99-nvidia.conf
 
+if [ ! -f /usr/libexec/rpm-ostree/wrapped/dracut ]; then
+    rpm-ostree cliwrap install-to-root /
+fi
 
+QUALIFIED_KERNEL="$(rpm -qa | grep -P 'kernel-(|'"$KERNEL_SUFFIX"'-)(\d+\.\d+\.\d+)' | sed -E 's/kernel-(|'"$KERNEL_SUFFIX"'-)//')"
+
+/usr/libexec/rpm-ostree/wrapped/dracut --no-hostonly --kver "$QUALIFIED_KERNEL" --reproducible -v --add ostree -f "/lib/modules/$QUALIFIED_KERNEL/initramfs.img"
+
+chmod 0600 "/lib/modules/$QUALIFIED_KERNEL/initramfs.img"
 ### Install packages
 
 # Packages can be installed from any enabled yum repo on the image.
